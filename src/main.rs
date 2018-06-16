f749fccdf5c449e14644016816c12a57f2999d1f
LEARAX <willculhane3@gmail.com>
2018-06-16 10:38:50 -0600

    Formatting

--- src/main.rs
+++ src/main.rs
@@ -17,37 +17,6 @@ fn handle_request(req: Request<Body>) -> BoxFut {
         (&Method::GET, "/_matrix/client/versions") => {
             server_administration::versions();
         }
-/*
- *         (&Method::GET, "/") => {
- *             *response.body_mut() = Body::from("Try POSTing data to /echo");
- *         }
- * 
- *         (&Method::POST, "/echo") => {
- *             *response.body_mut() = req.into_body();
- *         }
- * 
- *         (&Method::POST, "/echo/uppercase") => {
- *             let mapping = req.into_body().map(|chunk| {
- *                 chunk
- *                     .iter()
- *                     .map(|byte| byte.to_ascii_uppercase())
- *                     .collect::<Vec<u8>>()
- *             });
- * 
- *             *response.body_mut() = Body::wrap_stream(mapping);
- *         }
- * 
- *         (&Method::POST, "/echo/reversed") => {
- *             let reversed = req.into_body().concat2().map(move |chunk| {
- *                 let body = chunk.iter().rev().cloned().collect::<Vec<u8>>();
- *                 *response.body_mut() = Body::from(body);
- *                 response
- *             });
- * 
- *             return Box::new(reversed);
- *         }
- */
-
         _ => {
             *response.status_mut() = StatusCode::NOT_FOUND;
         }
