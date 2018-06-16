7c6ccb90fd301df42acf6e5e7ef7c46135ce1d64
LEARAX <willculhane3@gmail.com>
2018-06-15 22:10:52 -0600

    Make it a webserver

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate hyper;
+
+use hyper::rt::Future;
+use hyper::service::service_fn_ok;
+use hyper::{Body, Request, Response, Server};
+
 fn main() {
-    println!("Hello, world!");
+    let addr = ([127, 0, 0, 1], 3000).into();
+
+    let new_svc = || service_fn_ok(hello_world);
+
+    let server = Server::bind(&addr)
+        .serve(new_svc)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    hyper::rt::run(server);
+}
+
+fn hello_world(_req: Request<Body>) -> Response<Body> {
+    Response::new(Body::from("Hello world!"))
 }
