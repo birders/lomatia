8f200633fe0cedba9cd6c55c85b48ad5dd77c66c
LEARAX <willculhane3@gmail.com>
2018-06-19 15:58:49 -0600

    Some formatting

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,20 @@ extern crate futures_cpupool;
 extern crate qstring;
 #[macro_use]
 extern crate lazy_static;
-extern crate regex;
 extern crate bcrypt;
-extern crate tokio_postgres;
+extern crate regex;
 extern crate tokio_core;
+extern crate tokio_postgres;
 extern crate uuid;
 
 mod server_administration;
 mod user_data;
 
-use std::sync::Arc;
 use futures::future;
 use hyper::rt::Future;
 use hyper::service::Service;
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
+use std::sync::Arc;
 
 type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
 
@@ -30,39 +30,36 @@ mod error_code {
 
 struct ErrorBody<'a> {
     pub errcode: &'static str,
-    pub error: &'a str
+    pub error: &'a str,
 }
 impl<'a> ErrorBody<'a> {
     const UNRECOGNIZED: ErrorBody<'static> = ErrorBody {
         errcode: "M_UNRECOGNIZED",
-        error: "Unrecognized request"
+        error: "Unrecognized request",
     };
     const NOT_JSON: ErrorBody<'static> = ErrorBody {
         errcode: "M_NOT_JSON",
-        error: "Content not JSON."
+        error: "Content not JSON.",
     };
     const BAD_JSON: ErrorBody<'static> = ErrorBody {
         errcode: "M_BAD_JSON",
-        error: "Invalid JSON body."
+        error: "Invalid JSON body.",
     };
     const GUEST_ACCESS_FORBIDDEN: ErrorBody<'static> = ErrorBody {
         errcode: "M_GUEST_ACCESS_FORBIDDEN",
-        error: "Guest accounts are forbidden."
+        error: "Guest accounts are forbidden.",
     };
     const INVALID_USERNAME: ErrorBody<'static> = ErrorBody {
         errcode: "M_INVALID_USERNAME",
-        error: "The desired user ID is not a valid user name."
+        error: "The desired user ID is not a valid user name.",
     };
     const INTERNAL_ERROR: ErrorBody<'static> = ErrorBody {
         errcode: error_code::CHAT_LOMATIA_INTERNAL_ERROR,
-        error: "Internal server error"
+        error: "Internal server error",
     };
 
     pub fn new<'b>(errcode: &'static str, error: &'b str) -> ErrorBody<'b> {
-        ErrorBody {
-            errcode,
-            error
-        }
+        ErrorBody { errcode, error }
     }
 }
 impl<'a> ErrorBody<'a> {
@@ -70,9 +67,12 @@ impl<'a> ErrorBody<'a> {
         let mut resp = Response::new(Body::from(self.to_string()));
         *resp.status_mut() = match self.errcode {
             error_code::CHAT_LOMATIA_INTERNAL_ERROR => StatusCode::INTERNAL_SERVER_ERROR,
-            _ => StatusCode::BAD_REQUEST
+            _ => StatusCode::BAD_REQUEST,
         };
-        resp.headers_mut().insert(hyper::header::CONTENT_TYPE, hyper::header::HeaderValue::from_static(APPLICATION_JSON));
+        resp.headers_mut().insert(
+            hyper::header::CONTENT_TYPE,
+            hyper::header::HeaderValue::from_static(APPLICATION_JSON),
+        );
 
         resp
     }
@@ -89,7 +89,7 @@ impl<'a> ToString for ErrorBody<'a> {
 #[derive(Debug)]
 enum Error {
     DB(tokio_postgres::Error),
-    CanceledFuture
+    CanceledFuture,
 }
 
 impl From<futures::Canceled> for Error {
@@ -104,7 +104,10 @@ impl From<tokio_postgres::Error> for Error {
     }
 }
 
-fn run_on_main<R, E: From<futures::Canceled>, F: 'static + Future<Item=R, Error=E> + Send>(remote: &tokio_core::reactor::Remote, f: impl FnOnce(&tokio_core::reactor::Handle) -> F + Send + 'static) -> Box<Future<Item=R, Error=E> + Send> {
+fn run_on_main<R, E: From<futures::Canceled>, F: 'static + Future<Item = R, Error = E> + Send>(
+    remote: &tokio_core::reactor::Remote,
+    f: impl FnOnce(&tokio_core::reactor::Handle) -> F + Send + 'static,
+) -> Box<Future<Item = R, Error = E> + Send> {
     match remote.handle() {
         Some(handle) => Box::new(f(&handle)),
         None => {
@@ -123,7 +126,7 @@ const APPLICATION_JSON: &'static str = "application/json";
 pub struct LMServer {
     cpupool: Arc<futures_cpupool::CpuPool>,
     db_params: tokio_postgres::params::ConnectParams,
-    remote: tokio_core::reactor::Remote
+    remote: tokio_core::reactor::Remote,
 }
 
 impl Service for LMServer {
@@ -135,18 +138,23 @@ impl Service for LMServer {
     fn call(&mut self, req: Request<Body>) -> BoxFut {
         match (req.method(), req.uri().path()) {
             (&Method::GET, "/_matrix/client/versions") => {
-                let mut response = Response::new(Body::from(server_administration::versions().to_string()));
+                let mut response =
+                    Response::new(Body::from(server_administration::versions().to_string()));
                 *response.status_mut() = StatusCode::OK;
-                response.headers_mut().insert(hyper::header::CONTENT_TYPE, hyper::header::HeaderValue::from_static(APPLICATION_JSON));
+                response.headers_mut().insert(
+                    hyper::header::CONTENT_TYPE,
+                    hyper::header::HeaderValue::from_static(APPLICATION_JSON),
+                );
                 Box::new(future::ok(response))
             }
-            (&Method::POST, "/_matrix/client/r0/register") => {
-                user_data::register(self, req)
-            }
+            (&Method::POST, "/_matrix/client/r0/register") => user_data::register(self, req),
             _ => {
                 let mut response = Response::new(Body::from(ErrorBody::UNRECOGNIZED.to_string()));
                 *response.status_mut() = StatusCode::BAD_REQUEST;
-                response.headers_mut().insert(hyper::header::CONTENT_TYPE, hyper::header::HeaderValue::from_static(APPLICATION_JSON));
+                response.headers_mut().insert(
+                    hyper::header::CONTENT_TYPE,
+                    hyper::header::HeaderValue::from_static(APPLICATION_JSON),
+                );
                 Box::new(future::ok(response))
             }
         }
@@ -159,7 +167,9 @@ fn main() {
     let mut core = tokio_core::reactor::Core::new().unwrap();
 
     let cpupool = Arc::new(futures_cpupool::Builder::new().create());
-    let db_params = tokio_postgres::params::IntoConnectParams::into_connect_params(std::env::var("DATABASE_URL").expect("Missing DATABASE_URL")).unwrap();
+    let db_params = tokio_postgres::params::IntoConnectParams::into_connect_params(
+        std::env::var("DATABASE_URL").expect("Missing DATABASE_URL"),
+    ).unwrap();
     let remote = core.remote();
 
     let server = Server::bind(&addr)
@@ -167,10 +177,10 @@ fn main() {
             future::ok(LMServer {
                 cpupool: cpupool.clone(),
                 db_params: db_params.clone(),
-                remote: remote.clone()
+                remote: remote.clone(),
             })
         })
-    .map_err(|e| eprintln!("server error: {}", e));
+        .map_err(|e| eprintln!("server error: {}", e));
 
     println!("Listening on http://{}", addr);
     core.run(server);
--- src/user_data.rs
+++ src/user_data.rs
@@ -1,12 +1,12 @@
 use bcrypt;
-use serde_json;
 use hyper;
 use qstring;
+use serde_json;
 use tokio_postgres;
 use uuid;
 
-use hyper::{Body, Request, Response, StatusCode};
 use futures::{future, Future, Stream};
+use hyper::{Body, Request, Response, StatusCode};
 use regex::Regex;
 
 use {error_code, run_on_main, BoxFut, ErrorBody, LMServer, APPLICATION_JSON};
@@ -15,7 +15,8 @@ lazy_static! {
     static ref VALID_USERNAME_RE: Regex = Regex::new("^[a-z-.=_/0-9]+$").unwrap();
 }
 
-const REGISTER_QUERY: &'static str = "INSERT INTO users (id, localpart, passhash) VALUES ($1, $2, $3)";
+const REGISTER_QUERY: &'static str =
+    "INSERT INTO users (id, localpart, passhash) VALUES ($1, $2, $3)";
 
 pub fn register(server: &LMServer, req: Request<Body>) -> BoxFut {
     let cpupool = server.cpupool.clone();
@@ -23,69 +24,87 @@ pub fn register(server: &LMServer, req: Request<Body>) -> BoxFut {
     let remote = server.remote.clone();
 
     let query = qstring::QString::from(req.uri().query().unwrap_or(""));
-    Box::new(req.into_body().concat2()
-             .and_then(move |body| -> BoxFut {
-                 let body: Result<serde_json::Value, serde_json::Error> = serde_json::from_slice(&body);
-                 if let Err(err) = body {
-                     return match err.classify() {
-                         serde_json::error::Category::Syntax | serde_json::error::Category::Eof | serde_json::error::Category::Io => {
-                             Box::new(future::ok(ErrorBody::NOT_JSON.to_response()))
-                         },
-                         serde_json::error::Category::Data => Box::new(future::ok(ErrorBody::BAD_JSON.to_response())),
-                     }
-                 }
-                 let body = body.unwrap(); // errors are handled above, so unwrap should be okay
+    Box::new(req.into_body().concat2().and_then(move |body| -> BoxFut {
+        let body: Result<serde_json::Value, serde_json::Error> = serde_json::from_slice(&body);
+        if let Err(err) = body {
+            return match err.classify() {
+                serde_json::error::Category::Syntax
+                | serde_json::error::Category::Eof
+                | serde_json::error::Category::Io => {
+                    Box::new(future::ok(ErrorBody::NOT_JSON.to_response()))
+                }
+                serde_json::error::Category::Data => {
+                    Box::new(future::ok(ErrorBody::BAD_JSON.to_response()))
+                }
+            };
+        }
+        let body = body.unwrap(); // errors are handled above, so unwrap should be okay
 
-                 if let Some(auth) = body["auth"].as_object() {
-                     match query.get("kind") {
-                         Some("guest") => Box::new(future::ok(ErrorBody::GUEST_ACCESS_FORBIDDEN.to_response())),
-                         Some("user") => {
-                             let username = body["username"].as_str().unwrap_or("").to_owned();
-                             if !VALID_USERNAME_RE.is_match(&username) {
-                                 return Box::new(future::ok(ErrorBody::INVALID_USERNAME.to_response()));
-                             }
-                             let password = body["password"].to_string();
-                             println!("hashing password");
-                             Box::new(cpupool.spawn_fn(move || {
-                                 println!("hashing...");
-                                 bcrypt::hash(&password, bcrypt::DEFAULT_COST)
-                             })
-                                      .then(move |hash_res| -> BoxFut {
-                                          match hash_res {
-                                              Ok(hash) => {
-                                                  println!("{:?}", hash);
-                                                  Box::new(run_on_main(&remote, |handle| {
-                                                      tokio_postgres::Connection::connect(db_params, tokio_postgres::TlsMode::None, &handle)
-                                                          .and_then(|db| db.prepare(REGISTER_QUERY))
-                                                          .and_then(|(q, db)| {
-                                                              let id = uuid::Uuid::new_v4();
-                                                              let values: Vec<&tokio_postgres::types::ToSql> = vec![&id, &username, &hash];
-                                                              db.query(&q, &values)
-                                                                  .and_then(|_| {
-                                                                      id
-                                                                  })
-                                                          })
-                                                          .map_err(|(e, db)| e)
-                                                          .map_err(::Error::from)
-                                                  })
-                                                  .then(|res| {
-                                                      println!("{:?}", res);
-                                                      Ok(ErrorBody::INTERNAL_ERROR.to_response())
-                                                  }))
-                                              },
-                                              Err(err) => {
-                                                  eprintln!("{:?}", err);
-                                                  Box::new(future::ok(ErrorBody::INTERNAL_ERROR.to_response()))
-                                              }
-                                          }
-                                      }))
-                         },
-                         _ => Box::new(future::ok(ErrorBody::new(error_code::CHAT_LOMATIA_INVALID_PARAM, &format!("Invalid 'kind' value, must be either 'guest' or 'user'")).to_response()))
-                     }
-                 }
-                 else {
-                     let session_id = "_session_id"; // TODO randomly generate this
-                     let mut resp = Response::new(Body::from(json!({
+        if let Some(auth) = body["auth"].as_object() {
+            match query.get("kind") {
+                Some("guest") => {
+                    Box::new(future::ok(ErrorBody::GUEST_ACCESS_FORBIDDEN.to_response()))
+                }
+                Some("user") => {
+                    let username = body["username"].as_str().unwrap_or("").to_owned();
+                    if !VALID_USERNAME_RE.is_match(&username) {
+                        return Box::new(future::ok(ErrorBody::INVALID_USERNAME.to_response()));
+                    }
+                    let password = body["password"].to_string();
+                    println!("hashing password");
+                    Box::new(
+                        cpupool
+                            .spawn_fn(move || {
+                                println!("hashing...");
+                                bcrypt::hash(&password, bcrypt::DEFAULT_COST)
+                            })
+                            .then(move |hash_res| -> BoxFut {
+                                match hash_res {
+                                    Ok(hash) => {
+                                        println!("{:?}", hash);
+                                        Box::new(
+                                            run_on_main(&remote, |handle| {
+                                                tokio_postgres::Connection::connect(
+                                                    db_params,
+                                                    tokio_postgres::TlsMode::None,
+                                                    &handle,
+                                                ).and_then(|db| db.prepare(REGISTER_QUERY))
+                                                    .and_then(|(q, db)| {
+                                                        let id = uuid::Uuid::new_v4();
+                                                        let values: Vec<&tokio_postgres::types::ToSql> = vec![&id, &username, &hash];
+                                                        db.query(&q, &values).and_then(|_| id)
+                                                    })
+                                                    .map_err(|(e, db)| e)
+                                                    .map_err(::Error::from)
+                                            }).then(
+                                                |res| {
+                                                    println!("{:?}", res);
+                                                    Ok(ErrorBody::INTERNAL_ERROR.to_response())
+                                                },
+                                            ),
+                                        )
+                                    }
+                                    Err(err) => {
+                                        eprintln!("{:?}", err);
+                                        Box::new(future::ok(
+                                            ErrorBody::INTERNAL_ERROR.to_response(),
+                                        ))
+                                    }
+                                }
+                            }),
+                    )
+                }
+                _ => Box::new(future::ok(
+                    ErrorBody::new(
+                        error_code::CHAT_LOMATIA_INVALID_PARAM,
+                        &format!("Invalid 'kind' value, must be either 'guest' or 'user'"),
+                    ).to_response(),
+                )),
+            }
+        } else {
+            let session_id = "_session_id"; // TODO randomly generate this
+            let mut resp = Response::new(Body::from(
+                json!({
                          "flows": [
                          {
                              "stages": [
@@ -94,10 +113,14 @@ pub fn register(server: &LMServer, req: Request<Body>) -> BoxFut {
                          }
                          ],
                          "session": session_id
-                     }).to_string()));
-                     *resp.status_mut() = StatusCode::UNAUTHORIZED;
-                     resp.headers_mut().insert(hyper::header::CONTENT_TYPE, hyper::header::HeaderValue::from_static(APPLICATION_JSON));
-                     Box::new(future::ok(resp))
-                 }
-             }))
+                     }).to_string(),
+            ));
+            *resp.status_mut() = StatusCode::UNAUTHORIZED;
+            resp.headers_mut().insert(
+                hyper::header::CONTENT_TYPE,
+                hyper::header::HeaderValue::from_static(APPLICATION_JSON),
+            );
+            Box::new(future::ok(resp))
+        }
+    }))
 }
